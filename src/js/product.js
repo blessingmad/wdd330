4012a655fb5a7f75b86aa19271999766bab2d384
blessingmad <114036717+blessingmad@users.noreply.github.com>
2023-10-18 23:50:46 +0200

    compare codes with the instructor and make changes

--- src/js/product.js
+++ src/js/product.js
@@ -2,18 +2,4 @@ import { setLocalStorage, getParam } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 const productId = getParam("product");
-console.log(findProductById(productId))
-
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+productDetails(productId);
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,17 +1,20 @@
 import { findProductById } from "./productData.mjs";
+import {setLocalStorage} from "./utils.mjs";
 
-function productDetails(productId) {
+let product = {};
+
+export default async function productDetails(productId) {
+    product = await findProductById(productId);
+    renderProductDetails();
+    document.getElementById("addToCart").addEventListener("click", addToCart);
 
 }
-function addToCart() {
-    setLocalStorage("so-cart", product);
-
+function renderProductDetails() {
+    document.querySelector("#productName").innerText = product.Brand.Name;
+    document.querySelector("#productNameWithoutBrand").innerText = product.NameWithoutBrand;
+    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+    document.querySelector("#productColorName").innerText = product. Colors[0].ColorName;
+    document.querySelector("#productDescriptionHtmlSample").innerHTML = product.DescriptionHtmlSample;
+    document.querySelector("#addToCart").dataset.id = product.id;
 }
-async function addToCartHandler(e) {
-    const product = await findProductById(e.target.dataset.id);
-    addToCart(product);
-}
-// add listener to add to chart button
-document
-    .getElementById("addToCart")
-    .addEventListener("click", addToCartHandler);
